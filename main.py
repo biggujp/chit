d58eda7d2ae3a339c656c184152b244d326f5d50
Pornchai <63188163+biggujp@users.noreply.github.com>
2025-04-01 22:11:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main(page: ft.Page):
         t.value = f"Dropdown value is:  {dd.value}"
         page.update()
 
-    t = ft.Text(value="0")
+    t = ft.Text()
 
     dd = ft.DropdownM2(
     label="กรุณาเลือกแรงดันไฟฟ้า",
@@ -16,13 +16,14 @@ def main(page: ft.Page):
         ft.dropdownm2.Option("3"),   
         ],
     )
+
     tb1 = ft.TextField(label="กรอกค่า KW")
-    tb2 = ft.TextField(label="ค่ากระแสที่ทำคำนวนได้", read_only=True, value={t})
+    tb2 = ft.TextField(label="ค่ากระแสที่ทำคำนวนได้", read_only=True, value=t.value)
     tb3 = ft.TextField(label="With placeholder", hint_text="Please enter text here")
     tb4 = ft.TextField(label="With an icon", icon=ft.Icons.EMOJI_EMOTIONS)
     b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
 
-    page.add(dd,tb1,tb2,tb3,tb4,b)
+    page.add(dd,tb1,t,tb3,tb4,b)
 
 
 ft.app(main)
