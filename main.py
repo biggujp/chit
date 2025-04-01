2569ca8e2d8080db3b6899cdc0691881adff1f17
Pornchai <63188163+biggujp@users.noreply.github.com>
2025-04-01 22:00:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,27 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "TJCalc App by BIGGUSAN"
-    
+
+    def button_clicked(e):
+        t.value = f"Dropdown value is:  {dd.value}"
+        page.update()
+
+    t = ft.Text(value="0")
+
     dd = ft.DropdownM2(
-        label="กรุณาเลือกแรงดันไฟฟ้า",
+    label="กรุณาเลือกแรงดันไฟฟ้า",
     options=[
-        ft.dropdownm2.Option("1P100V"),
-        ft.dropdownm2.Option("3P200V"),   
+        ft.dropdownm2.Option("1"),
+        ft.dropdownm2.Option("3"),   
         ],
     )
     tb1 = ft.TextField(label="กรอกค่า KW")
-    tb2 = ft.TextField(label="ค่ากระแสที่ทำคำนวนได้", read_only=True, value="0.0")
+    tb2 = ft.TextField(label="ค่ากระแสที่ทำคำนวนได้", read_only=True, value={t})
     tb3 = ft.TextField(label="With placeholder", hint_text="Please enter text here")
     tb4 = ft.TextField(label="With an icon", icon=ft.Icons.EMOJI_EMOTIONS)
-    page.add(dd,tb1,tb2,tb3,tb4)
+    b = ft.ElevatedButton(text="Submit", on_click=button_clicked)
+
+    page.add(dd,tb1,tb2,tb3,tb4,b)
 
 
 ft.app(main)
